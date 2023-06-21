f35e0c6a6a48a5dce0c3367246d3630e7c03ab6a
azriel1rf <azriel.1rf@gmail.com>
2023-06-21 18:17:05 +0900

    merge the functions into a single function.

--- Lesson 10 - Prime and composite numbers/flags.py
+++ Lesson 10 - Prime and composite numbers/flags.py
@@ -1,48 +1,33 @@
-def find_peaks(heights):
-    num_peaks = len(heights)
-    peaks = [False] * num_peaks
+def solution(heights):
+    N = len(heights)
+    if N < 3:
+        return 0
 
-    for i in range(1, num_peaks - 1):
+    peaks = [False] * N
+    for i in range(1, N - 1):
         if heights[i] > heights[i - 1] and heights[i] > heights[i + 1]:
             peaks[i] = True
 
-    return peaks
-
-
-def find_next_peak(heights, peaks):
-    next_peak = [0] * len(heights)
+    next_peak = [0] * N
     next_peak[-1] = -1
-
-    for i in range(len(heights) - 2, -1, -1):
+    for i in range(N - 1)[::-1]:
         if peaks[i]:
             next_peak[i] = i
         else:
             next_peak[i] = next_peak[i + 1]
 
-    return next_peak
-
-
-def solution(heights):
-    if len(heights) < 3:
-        return 0
-
-    peaks = find_peaks(heights)
-    next_peak = find_next_peak(heights, peaks)
-
     max_flags = 0
-    for distance in range(1, len(heights)):
-        if distance * (distance - 1) > len(heights):
-            break
-
+    distance = 1
+    while (distance - 1) * distance <= N:
         num_flags = 0
         pos = 0
-        while pos < len(heights) and num_flags < distance:
+        while pos < N and num_flags < distance:
             pos = next_peak[pos]
             if pos == -1:
                 break
             pos += distance
             num_flags += 1
-
         max_flags = max(max_flags, num_flags)
+        distance += 1
 
     return max_flags
